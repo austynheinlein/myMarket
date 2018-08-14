ab04249f3995d0a8699b90da65febe037dbf42c8
Katelynn Weingart <kweingart@Andrews-iMac.attlocal.net>
2018-08-14 13:03:30 -0400

    updated heroku database connection

--- app/models/location.rb
+++ app/models/location.rb
@@ -4,8 +4,8 @@ class Location
   #                      SET UP
   # ==================================================
   # connect to postgres
-  if(ENV['https://my-markets.herokuapp.com/'])
-        uri = URI.parse(ENV['https://my-markets.herokuapp.com/'])
+  if(ENV['DATABASE_URL'])
+        uri = URI.parse(ENV['DATABASE_URL'])
         DB = PG.connect(uri.hostname, uri.port, nil, nil, uri.path[1..-1], uri.user, uri.password)
   else
       DB = PG.connect(host: "localhost", port: 5432, dbname: 'myMarket_development')
